9ca7f3aeab10d16caf28c199fd8cefd325b91f10
Yota Toyama <raviqqe@gmail.com>
2020-11-27 06:50:44 +0000

    Add github actions

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ const eslintConfig: Linter.Config = {
     "jest/globals": true,
   },
   extends: [
+    "react-app",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
