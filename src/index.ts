1c055bae6f8800b8a63a1d9b3c900c2dd9d5ba07
Yota Toyama <raviqqe@gmail.com>
2023-05-18 18:06:49 -0700

    Ignore test.ts file (#635)

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ const eslintConfig: Linter.Config = {
   overrides: [
     {
       files: [
+        "**/test.ts{,x}",
         "**/*.test.ts{,x}",
         "**/__tests__/*.ts{,x}",
         "**/test/**/*.ts{,x}",
