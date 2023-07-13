ffe15016a2638f928adec7cdbfc8857adaf54fb8
Yota Toyama <raviqqe@gmail.com>
2023-07-13 22:45:26 +1000

    Add stylistic configurations (#685)

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,9 @@ const eslintConfig: Linter.Config = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic",
+    "plugin:@typescript-eslint/stylistic-type-checked",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
@@ -41,7 +43,7 @@ const eslintConfig: Linter.Config = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: { jsx: true },
-    project: "tsconfig.json",
+    project: true,
     sourceType: "module",
   },
   plugins: [
