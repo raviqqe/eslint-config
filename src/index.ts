c119ad1345caef25580d093eabe367f830ce32fe
Yota Toyama <raviqqe@gmail.com>
2023-07-13 22:43:43 +1000

    Add stylistic configurations

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,9 @@ const eslintConfig: Linter.Config = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic",
+    "plugin:@typescript-eslint/stylistic-type-checked",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
