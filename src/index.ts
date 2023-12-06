5191f889b5be8a6878e7281d3afcd16b84846813
Yota Toyama <raviqqe@gmail.com>
2023-12-06 19:24:53 +0900

    Use flat config

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { type Linter } from "eslint";
 
 /* eslint-disable @typescript-eslint/naming-convention */
 
-const eslintConfig: Linter.Config = {
+const config = {
   env: {
     browser: true,
     es6: true,
@@ -56,120 +56,6 @@ const eslintConfig: Linter.Config = {
     "jsx-a11y",
     "eslint-comments",
   ],
-  rules: {
-    "@typescript-eslint/adjacent-overload-signatures": "error",
-    "@typescript-eslint/array-type": "error",
-    "@typescript-eslint/consistent-indexed-object-style": "error",
-    "@typescript-eslint/consistent-type-assertions": "error",
-    "@typescript-eslint/consistent-type-definitions": "error",
-    "@typescript-eslint/consistent-type-exports": [
-      "error",
-      { fixMixedExportsWithInlineTypeSpecifier: true },
-    ],
-    "@typescript-eslint/consistent-type-imports": [
-      "error",
-      { disallowTypeAnnotations: false, fixStyle: "inline-type-imports" },
-    ],
-    "@typescript-eslint/explicit-member-accessibility": [
-      "error",
-      {
-        accessibility: "explicit",
-        overrides: {
-          accessors: "explicit",
-          constructors: "off",
-          methods: "explicit",
-          parameterProperties: "explicit",
-          properties: "explicit",
-        },
-      },
-    ],
-    "@typescript-eslint/explicit-module-boundary-types": "error",
-    "@typescript-eslint/member-ordering": "error",
-    "@typescript-eslint/naming-convention": [
-      "error",
-      {
-        format: ["camelCase"],
-        leadingUnderscore: "allow",
-        selector: "default",
-      },
-      {
-        format: ["camelCase", "PascalCase", "UPPER_CASE"],
-        leadingUnderscore: "allow",
-        selector: "variable",
-      },
-      {
-        format: ["camelCase", "PascalCase"],
-        selector: "import",
-      },
-      {
-        format: ["PascalCase"],
-        selector: "enumMember",
-      },
-      {
-        format: ["PascalCase"],
-        selector: "typeLike",
-      },
-    ],
-    "@typescript-eslint/no-empty-interface": [
-      "error",
-      { allowSingleExtends: true },
-    ],
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/no-meaningless-void-operator": "error",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      { checksVoidReturn: { attributes: false, properties: false } },
-    ],
-    "@typescript-eslint/no-require-imports": "error",
-    "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
-    "@typescript-eslint/no-unnecessary-condition": [
-      "error",
-      { allowConstantLoopConditions: true },
-    ],
-    "@typescript-eslint/no-unnecessary-type-arguments": "error",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-    ],
-    "@typescript-eslint/prefer-optional-chain": "error",
-    "@typescript-eslint/prefer-readonly": "error",
-    "@typescript-eslint/prefer-return-this-type": "error",
-    "@typescript-eslint/prefer-ts-expect-error": "error",
-    "arrow-body-style": "error",
-    curly: "error",
-    "dot-notation": "error",
-    "eslint-comments/no-unused-disable": "error",
-    "import/extensions": ["error", "always", { ts: "never", tsx: "never" }],
-    "import/no-cycle": "error",
-    "import/no-default-export": "error",
-    "import/no-named-as-default": "error",
-    "import/no-unused-modules": "error",
-    "import/order": ["error", { alphabetize: { order: "asc" } }],
-    "lines-between-class-members": [
-      "error",
-      "always",
-      { exceptAfterSingleLine: true },
-    ],
-    "no-console": "error",
-    "no-duplicate-imports": "error",
-    "no-else-return": "error",
-    "no-lonely-if": "error",
-    "no-return-await": "error",
-    "no-useless-return": "error",
-    "object-shorthand": "error",
-    quotes: [
-      "error",
-      "double",
-      { allowTemplateLiterals: false, avoidEscape: true },
-    ],
-    "react-hooks/exhaustive-deps": "error",
-    "react/jsx-boolean-value": "error",
-    "react/jsx-curly-brace-presence": "error",
-    "react/jsx-no-useless-fragment": "error",
-    "react/jsx-sort-props": "error",
-    "react/no-unescaped-entities": "off",
-    "sort-keys": "error",
-  },
   settings: {
     "import/resolver": {
       typescript: true,
@@ -180,6 +66,125 @@ const eslintConfig: Linter.Config = {
   },
 };
 
+const configurations: Linter.FlatConfig[] = [
+  {
+    rules: {
+      "@typescript-eslint/adjacent-overload-signatures": "error",
+      "@typescript-eslint/array-type": "error",
+      "@typescript-eslint/consistent-indexed-object-style": "error",
+      "@typescript-eslint/consistent-type-assertions": "error",
+      "@typescript-eslint/consistent-type-definitions": "error",
+      "@typescript-eslint/consistent-type-exports": [
+        "error",
+        { fixMixedExportsWithInlineTypeSpecifier: true },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { disallowTypeAnnotations: false, fixStyle: "inline-type-imports" },
+      ],
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        {
+          accessibility: "explicit",
+          overrides: {
+            accessors: "explicit",
+            constructors: "off",
+            methods: "explicit",
+            parameterProperties: "explicit",
+            properties: "explicit",
+          },
+        },
+      ],
+      "@typescript-eslint/explicit-module-boundary-types": "error",
+      "@typescript-eslint/member-ordering": "error",
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          format: ["camelCase"],
+          leadingUnderscore: "allow",
+          selector: "default",
+        },
+        {
+          format: ["camelCase", "PascalCase", "UPPER_CASE"],
+          leadingUnderscore: "allow",
+          selector: "variable",
+        },
+        {
+          format: ["camelCase", "PascalCase"],
+          selector: "import",
+        },
+        {
+          format: ["PascalCase"],
+          selector: "enumMember",
+        },
+        {
+          format: ["PascalCase"],
+          selector: "typeLike",
+        },
+      ],
+      "@typescript-eslint/no-empty-interface": [
+        "error",
+        { allowSingleExtends: true },
+      ],
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-meaningless-void-operator": "error",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        { checksVoidReturn: { attributes: false, properties: false } },
+      ],
+      "@typescript-eslint/no-require-imports": "error",
+      "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
+      "@typescript-eslint/no-unnecessary-condition": [
+        "error",
+        { allowConstantLoopConditions: true },
+      ],
+      "@typescript-eslint/no-unnecessary-type-arguments": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/prefer-optional-chain": "error",
+      "@typescript-eslint/prefer-readonly": "error",
+      "@typescript-eslint/prefer-return-this-type": "error",
+      "@typescript-eslint/prefer-ts-expect-error": "error",
+      "arrow-body-style": "error",
+      curly: "error",
+      "dot-notation": "error",
+      "eslint-comments/no-unused-disable": "error",
+      "import/extensions": ["error", "always", { ts: "never", tsx: "never" }],
+      "import/no-cycle": "error",
+      "import/no-default-export": "error",
+      "import/no-named-as-default": "error",
+      "import/no-unused-modules": "error",
+      "import/order": ["error", { alphabetize: { order: "asc" } }],
+      "lines-between-class-members": [
+        "error",
+        "always",
+        { exceptAfterSingleLine: true },
+      ],
+      "no-console": "error",
+      "no-duplicate-imports": "error",
+      "no-else-return": "error",
+      "no-lonely-if": "error",
+      "no-return-await": "error",
+      "no-useless-return": "error",
+      "object-shorthand": "error",
+      quotes: [
+        "error",
+        "double",
+        { allowTemplateLiterals: false, avoidEscape: true },
+      ],
+      "react-hooks/exhaustive-deps": "error",
+      "react/jsx-boolean-value": "error",
+      "react/jsx-curly-brace-presence": "error",
+      "react/jsx-no-useless-fragment": "error",
+      "react/jsx-sort-props": "error",
+      "react/no-unescaped-entities": "off",
+      "sort-keys": "error",
+    },
+  },
+];
+
 /* eslint-enable @typescript-eslint/naming-convention */
 
-module.exports = eslintConfig;
+export default configurations;
